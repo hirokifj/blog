03bb1c585b60ed9413ac49647a2aa9163257fe20
hirokifj <hf.rowkey313@gmail.com>
2022-01-23 21:23:11 +0900

    chore: use stylelint-config-prettier

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recess-order',
+    'stylelint-config-prettier',
+  ],
   plugins: ['stylelint-order'],
   customSyntax: '@stylelint/postcss-css-in-js',
   ignoreFiles: ['**/node_modules/**'],

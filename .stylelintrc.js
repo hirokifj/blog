05dd3382a4eb158cbd7fd01aada50a17f73256c9
hirokifj <hf.rowkey313@gmail.com>
2022-01-23 21:20:12 +0900

    chore: use @stylelint/postcss-css-in-js to avoid linting error in .tsx file

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
   plugins: ['stylelint-order'],
+  customSyntax: '@stylelint/postcss-css-in-js',
   ignoreFiles: ['**/node_modules/**'],
   rules: {
     'string-quotes': 'single',

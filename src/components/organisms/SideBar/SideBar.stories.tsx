f7a6695664b0b4f51114df265e507620fd2436c8
hirokifj <hf.rowkey313@gmail.com>
2022-02-07 21:11:48 +0900

    feat: add SideBar component

---
+++ src/components/organisms/SideBar/SideBar.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Meta, Story } from '@storybook/react'
+import { SideBar } from './SideBar'
+
+export default {
+  title: 'organisms/SideBar',
+  component: SideBar,
+  decorators: [(story) => <div style={{ maxWidth: '290px' }}>{story()}</div>],
+} as Meta
+
+type Template = Story
+
+const Template: Template = (args) => <SideBar {...args} />
+
+export const Default: Template = Template.bind({})
---
+++ src/components/organisms/SideBar/SideBar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ProfileBox } from '@/components/organisms/ProfileBox'
+
+export const SideBar: React.VFC = () => {
+  return (
+    <div>
+      <ProfileBox />
+    </div>
+  )
+}

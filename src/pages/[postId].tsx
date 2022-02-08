5b2f641984ee3e878f1c9c59c3861c58649b89e0
hirokifj <hf.rowkey313@gmail.com>
2022-02-08 18:35:32 +0900

    feat: add post page

---
+++ src/pages/[postId].tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { NextPage, GetStaticProps, GetStaticPaths } from 'next'
+import { ParsedUrlQuery } from 'querystring'
+
+import { Post, fetchPostById, fetchPostIds } from '@/features/posts'
+import { PostTemplate } from '@/components/templates/PostTemplate'
+
+type Props = {
+  post: Post
+}
+
+interface Params extends ParsedUrlQuery {
+  postId: string
+}
+
+const PostPage: NextPage<Props> = ({ post }) => {
+  return <PostTemplate post={post} />
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: fetchPostIds().map((postId) => ({
+      params: {
+        postId,
+      },
+    })),
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps<Props, Params> = async ({
+  params,
+}) => {
+  if (!params?.postId) {
+    throw new Error('Invalid parameter: postId is not given')
+  }
+
+  const post = await fetchPostById(params.postId)
+
+  return {
+    props: {
+      post,
+    },
+  }
+}
+
+export default PostPage
